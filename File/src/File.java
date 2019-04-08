e97dc5324510e5e59237b617876fb584c2857881
mazharkafi004 <43183733+mazharkafi004@users.noreply.github.com>
2019-04-08 11:24:34 +0600

    Add files via upload

---
+++ File/src/File.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class File {
+    public static void main(String[] args) {
+        String a;
+        BufferedReader br = null;
+        BufferedWriter writer=null;
+        try {
+            br = new BufferedReader(new FileReader("skil.txt"),10);
+            writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write('\n');
+            String s;
+            String word;
+            int count ;
+            while((s = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(s," ?."); 
+                String str= new String();
+                while(st.hasMoreTokens())
+                {
+                    word=st.nextToken();
+                    count= 1;
+                    while(st.hasMoreTokens())
+                    {
+                        String nxtword=st.nextToken();
+                        if(nxtword.equals(word))
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            str= str + nxtword + " " ;
+                        }
+                    }
+                    System.out.println("'" + word + "'" + " is " + count + " times.");
+                    writer.append("'" + word + "'" + " is " + count + " times.\n");
+                    st = new StringTokenizer(str," ?.");
+                    str= new String();
+                }
+                }
+                writer.close();
+            }catch(IOException e) {
+            e.printStackTrace();
+            } finally {
+                try {
+                    br.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+    }
+}
